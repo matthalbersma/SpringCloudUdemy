76a4dfabc7f0c5e0ba94d0341c0287d358c8b20b
Matthew Halbersma <mhalbers@nerdery.com>
2017-05-25 11:30:00 -0500

    Changed a few things

--- lab-1/boot-solution/src/main/java/demo/repository/TeamRepository.java
+++ lab-1/boot-solution/src/main/java/demo/repository/TeamRepository.java
@@ -1,11 +1,15 @@
 package demo.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import demo.domain.Team;
 
+import java.util.List;
+
 @RestResource(path="teams", rel="team")
 public interface TeamRepository extends CrudRepository<Team,Long>{
+    List<Team> findByName(@Param("name") String name);
 
 }
